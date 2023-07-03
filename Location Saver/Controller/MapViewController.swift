055a4c8cc6cdaaca4f5704df5bcd559c4e5df1ba
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-07-04 01:27:33 +0300

    adding market to view controller an config it

--- Location Saver/Controller/MapViewController.swift
+++ Location Saver/Controller/MapViewController.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Map"
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
