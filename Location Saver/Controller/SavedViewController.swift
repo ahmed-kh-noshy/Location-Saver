dc5da7ccc5b2dc61ec7a4cc2d5f897d1f6786c51
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-07-04 02:40:44 +0300

    adding table view to saved view controller

--- Location Saver/Controller/SavedViewController.swift
+++ Location Saver/Controller/SavedViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class SavedViewController: UIViewController {
 
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +25,6 @@ class SavedViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
