eb912f35a3a009a8f1c1c1cdfc3785f5907c40f5
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-07-04 11:08:42 +0300

    adding and config places model to apply singleton

---
+++ Location Saver/Model/Places.swift
@@ -0,0 +1,16 @@
+//
+//  Places.swift
+//  Location Saver
+//
+//  Created by ahmed khaled on 04/07/2023.
+//
+
+import Foundation
+
+
+class Places{
+    static shared = Places()
+    var landmarks: [Landmark] = []
+    
+    private init{}
+}
